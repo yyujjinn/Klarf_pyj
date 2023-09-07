eae37cf6b25672a9093a3f7e94c1f21df442d94c
yyujjinn <yujin@ati2000.co.kr>
2023-09-07 15:20:31 +0900

    Day 14
    ## PR Type 🤩
    - 🤩 기능 추가 또는 삭제
    - [ ] 버그 수정
    - [ ] 주석추가 또는 수정
    - [] 문서 수정
    - [ ] 코드 리펙토링
    - [ ] 코드에 영향을 주지 않는 변경사항
    - [ ] 의존성, 환경 변수, 빌드 관련 코드 업데이트
    
    ## 주요 변경 사항 🔑
    - Bar UI 생성하여 확대/축소 기능 추가
    - Defect Image Ruler 기능 추가

--- Klarf_pyj2/ViewModel/DefectImageViewModel.cs
+++ Klarf_pyj2/ViewModel/DefectImageViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Media.Imaging;
 using System.ComponentModel;
 using System.IO;
+using System.Windows.Input;
 
 namespace Klarf
 {
@@ -14,6 +15,7 @@ namespace Klarf
     {
         #region [상수]
         public event PropertyChangedEventHandler PropertyChanged;
+        private bool isRightButtonDown = false;
 
         #endregion
 
@@ -21,10 +23,18 @@ namespace Klarf
         MainModel mainModel;
         private TiffBitmapDecoder tiffDecoder;
         private BitmapFrame loadedImage;
+        private Point startPoint;
+        private Point endPoint;
+        private bool isDragging;
+        private string rulerLength;
 
         #endregion
 
         #region [속성]
+        public ICommand MouseRightButtonDownCommand { get; }
+        public ICommand MouseMoveCommand { get; }
+        public ICommand MouseRightButtonUpCommand { get; }
+
         public BitmapFrame LoadedImage
         {
             get { return loadedImage; }
@@ -38,6 +48,49 @@ namespace Klarf
             }
         }
 
+        public string RulerLength
+        {
+            get { return rulerLength; }
+            set
+            {
+                if (rulerLength != value)
+                {
+                    rulerLength = value;
+                    OnPropertyChanged(nameof(RulerLength));
+                }
+            }
+        }
+
+        public Point StartPoint
+        {
+            get { return startPoint; }
+            set
+            {
+                startPoint = value;
+                OnPropertyChanged(nameof(StartPoint));
+            }
+        }
+
+        public Point EndPoint
+        {
+            get { return endPoint; }
+            set
+            {
+                endPoint = value;
+                OnPropertyChanged(nameof(EndPoint));
+            }
+        }
+
+        public bool IsDragging
+        {
+            get { return isDragging; }
+            set
+            {
+                isDragging = value;
+                OnPropertyChanged(nameof(IsDragging));
+            }
+        }
+
         #endregion
 
         #region [생성자]
@@ -45,6 +98,9 @@ namespace Klarf
         {
             MainModel.Instance.PropertyChanged += MainModel_PropertyChanged;
             mainModel = MainModel.Instance;
+            MouseRightButtonDownCommand = new RelayCommand<object>(OnRightDown);
+            MouseMoveCommand = new RelayCommand<object>(OnMouseMove);
+            MouseRightButtonUpCommand = new RelayCommand<object>(OnRightUp);
         }
 
         #endregion
@@ -81,6 +137,41 @@ namespace Klarf
             {
                 LoadedImage = tiffDecoder.Frames[mainModel.DefectImage.currentImageIndex];
             }
+        }
+
+        private void OnRightDown(object parameter)
+        {
+            isRightButtonDown = true;
+            startPoint = Mouse.GetPosition(null);
+            isDragging = true;
+        }
+
+        private void OnMouseMove(object parameter)
+        {
+            if (isRightButtonDown)
+            {
+                endPoint = Mouse.GetPosition(null);
+                RulerLength = CalculateRulerLength();
+            }
+        }
+
+        private void OnRightUp(object parameter)
+        {
+            isRightButtonDown = false;
+            isDragging = false;
+        }
+
+        private string CalculateRulerLength()
+        {
+            double deltaX = endPoint.X - startPoint.X;
+            double deltaY = endPoint.Y - startPoint.Y;
+
+            double length = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            double lengthMicrometers = length * 0.266;
+
+            string lengthMicrometersText = $"{lengthMicrometers:F2} µm";
+
+            return lengthMicrometersText;
         }
 
         #endregion
